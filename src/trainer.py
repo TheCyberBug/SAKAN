862ac6156b9936cca681d5b8cfb1b4cf30caf4e3
Kaloyan Tsankov <kaloqn.cankov@gmail.com>
2024-08-31 08:31:09 +0100

    Update trainer.py
    Add comments

--- src/trainer.py
+++ src/trainer.py
@@ -6,6 +6,11 @@ from tqdm import tqdm
 
 
 class SentimentTrainer:
+    """
+    Training and evaluating the sentiment analysis model with PyTorch.
+
+    Handles the training loop, data preparation, and evaluation.
+    """
     def __init__(self, model, device, batch_size=32):
         self.model = model
         self.device = device
@@ -15,6 +20,18 @@ class SentimentTrainer:
         self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=0.9)
 
     def prepare_data(self, X_train, y_train, X_test, y_test):
+        """
+        Prepare the training and testing data.
+
+        Converts numpy arrays to PyTorch tensors and creates DataLoader
+        objects for both training and testing datasets.
+
+        Args:
+            X_train (np.array): Training features.
+            y_train (np.array): Training labels.
+            X_test (np.array): Testing features.
+            y_test (np.array): Testing labels.
+        """
         X_train_tensor = torch.tensor(X_train.tolist()).to(self.device)
         X_test_tensor = torch.tensor(X_test.tolist()).to(self.device)
         y_train_tensor = torch.tensor(y_train.tolist()).to(self.device)
@@ -27,11 +44,27 @@ class SentimentTrainer:
         self.test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
 
     def calculate_accuracy(self, preds, labels):
+        """
+        Calculate the accuracy.
+
+        Args:
+            preds (torch.Tensor): Model predictions.
+            labels (torch.Tensor): True labels.
+
+        Returns:
+            int: Number of correct predictions.
+        """
         _, predicted = torch.max(preds, 1)
         correct = (predicted == labels).sum().item()
         return correct
 
     def evaluate(self):
+        """
+        Evaluate the model on the test dataset.
+
+        Returns:
+            tuple: A tuple containing (accuracy, average_loss).
+        """
         self.model.eval()
         correct = 0
         total = 0
@@ -48,6 +81,16 @@ class SentimentTrainer:
         return accuracy, avg_loss
 
     def train(self, num_epochs):
+        """
+        Train the model for a specified number of epochs.
+        Handles the entire training loop, including evaluation after each epoch.
+
+        Args:
+            num_epochs (int): Number of epochs to train for.
+
+        Returns:
+            list: A list of tuples containing (validation_accuracy, validation_loss) for each epoch.
+        """
         model_history = []
 
         for epoch in range(num_epochs):
